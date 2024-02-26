d37bef156e0bf600dd7333b0c7dbd602d444a4e1
anthonymanciny <141266961+anthonymanciny@users.noreply.github.com>
2024-02-26 09:58:11 -0400

    .

--- js/game.js
+++ js/game.js
@@ -13,10 +13,60 @@ const characters = [
     'scroopy',
   ];
 
-const createElement = (tag, className) => {
-    const element = document.createElement(tag);
-    element.className = className;
+const createElement =(tag, className) => {
+    const element =document.createElement(tag);
+    element.className =className;
     return element;
+}
+
+let firstCard = '';
+let secondCard = '';
+
+const checkCards = () =>{
+
+    const firstCharacter = firstCard.getAttribute('data-character')
+    const secondCharacter = secondCard.getAttribute('data-character')
+    if(firstCharacter=== secondCharacter){
+        firstCard = '';
+        secondCard = '';
+
+    }else{
+
+        setTimeout(() =>{
+
+            firstCard.classList.remove('reveal-card');
+            secondCard.classList.remove('reveal-card');
+
+            firstCard = '';
+            secondCard = '';
+
+        },500);
+}
+}
+const revealCard = ({target}) =>{
+
+    if(target.parentNode.className.includes('reveal-card')){
+        return
+    }
+
+
+    if(firstCard == ''){
+
+        target.parentNode.classList.add('reveal-card');
+        firstCard = target.parentNode;
+
+    }else if (secondCard == ''){
+
+        target.parentNode.classList.add('reveal-card');
+        secondCard = target.parentNode
+
+        checkCards();
+
+
+
+    }
+
+
 }
 
 const createCard = (character) => {
@@ -29,6 +79,9 @@ const createCard = (character) => {
 
     card.appendChild(front);
     card.appendChild(back);
+
+    card.addEventListener('click', revealCard)
+    card.setAttribute('data-character', character)
 
     return card
 

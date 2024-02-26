d81d68ea200c178107968a7b052e9230f5904ac1
anthonymanciny <141266961+anthonymanciny@users.noreply.github.com>
2024-02-26 09:11:43 -0400

    .

--- js/game.js
+++ js/game.js
@@ -25,9 +25,7 @@ const createCard = (character) => {
     const front = createElement('div','face front');
     const back = createElement('div','face back');
 
-    front.style.backgroundImage = `url {'../images/${character}.png'}`;
-
-   
+    front.style.backgroundImage = `url('/images/${character}.png')`;
 
     card.appendChild(front);
     card.appendChild(back);
@@ -38,8 +36,12 @@ const createCard = (character) => {
 }
 
 const loadGame= ()=>{
-    characters.forEach((character) => {
 
+    const duplicateCharacters =[... characters, ...characters]
+
+    const shuffledArray = duplicateCharacters.sort(() => Math.random() - 0.5);
+
+    shuffledArray.forEach((character) => {
      const card = createCard(character);
      grid.appendChild(card);
 

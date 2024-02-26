1e2fedefa39433dbba34075cc774f6d4b772a77d
anthonymanciny <141266961+anthonymanciny@users.noreply.github.com>
2024-02-26 10:19:58 -0400

    .

--- js/game.js
+++ js/game.js
@@ -22,13 +22,28 @@ const createElement =(tag, className) => {
 let firstCard = '';
 let secondCard = '';
 
+const checkEndGame = () =>{
+    const disabledCards = document.querySelectorAll('.disable-card');
+
+    if(disabledCards.length === 20){
+        alert('PARABÉNS, TU É O BICHÃO MESMO!')
+    }
+}
+
 const checkCards = () =>{
 
     const firstCharacter = firstCard.getAttribute('data-character')
     const secondCharacter = secondCard.getAttribute('data-character')
-    if(firstCharacter=== secondCharacter){
+
+    if(firstCharacter === secondCharacter){
+
+        firstCard.firstChild.classList.add('disabled-card');
+        secondCard.firstChild.classList.add('disabled-card');
+
         firstCard = '';
         secondCard = '';
+
+        checkEndGame();
 
     }else{
 
